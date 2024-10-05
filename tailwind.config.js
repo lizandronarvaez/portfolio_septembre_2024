94c61648caba512b5c19ed498cc71d21915f5987
Lizandro <lizandrojesus13@hotmail.com>
2024-10-05 13:51:53 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,6 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      keyframes: {
-        bounceY: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(100%)' },  // Cambia 100% si quieres que baje más o menos
-        },
-      },
-      animation: {
-        bounceY: 'bounceY 2s ease-in-out infinite',
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 }
