2344cf3b38829d10ac93cbddc59ad72356c5a74a
Lizandro <lizandrojesus13@hotmail.com>
2024-10-05 17:30:50 +0200

    Ordenando imagenes en carpetas

---
+++ public/img/index.js
@@ -0,0 +1,32 @@
+// Certificados
+import React_MicroCompanys from "./certificados/react_microempresas.jpg";
+import Solid from "./certificados/solid.jpg";
+import Sql from "./certificados/sql.jpg";
+import SpringbBoot from "./certificados/springboot.jpg";
+import React from "./certificados/react.jpg";
+import Nest from "./certificados/nest.jpg";
+import Git_Github from "./certificados/git_github.jpg";
+import ScrumStudy from "./certificados/ScrumStudy.png";
+import JsAdvanced from "./certificados/Certificado_Ceinpro.png";
+import TailwindCss from "./certificados/tailwind_curso.jpg"
+
+// Tech
+import Frontend from "./tech/frontend.webp";
+import Backend from "./tech/backend.png";
+import Database from "./tech/database_1.jpg";
+
+export {
+    React_MicroCompanys,
+    Solid,
+    Sql,
+    SpringbBoot,
+    React,
+    Nest,
+    Git_Github,
+    ScrumStudy,
+    JsAdvanced,
+    TailwindCss,
+    Frontend,
+    Backend,
+    Database
+}
