bdd2cb227e6e95d60253469b299c4483ffe59164
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-11-23 16:25:10 +0100

    chore: elimino div innecesario

--- src/components/TechProfile.jsx
+++ src/components/TechProfile.jsx
@@ -3,23 +3,19 @@ import { TechProfileData } from "../data/TechProfileData"
 export const TechProfile = () => {
 
     return (
-        <div id="tech">
+        <div className="md:grid md:grid-cols-3 gap-6 mb-20">
 
-            <div className="md:grid md:grid-cols-3 gap-6 mb-20">
-
-                {
-                    TechProfileData.map((tech, i) => (
-                        <div key={i} className=" p-5">
-                            {/* div imagen */}
-                            <h5 className="text-4xl text-center text-stone-600">{tech.title}</h5>
-                            <div>
-                                <img className="w-1/2 mx-auto brightness-110" src={tech.img} alt={tech.img} />
-                            </div>
+            {
+                TechProfileData.map((tech, i) => (
+                    <div key={i} className=" p-5">
+                        {/* div imagen */}
+                        <h5 className="text-4xl text-center text-stone-600">{tech.title}</h5>
+                        <div>
+                            <img className="w-1/2 mx-auto brightness-110" src={tech.img} alt={tech.img} />
                         </div>
-                    ))
-                }
-
-            </div>
+                    </div>
+                ))
+            }
 
         </div>
     )
